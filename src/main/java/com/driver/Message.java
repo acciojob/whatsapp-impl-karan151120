24168936ede179602b913ae1ba55207ce315dde5
karan151120 <karantulsani28@gmail.com>
2023-04-01 11:43:23 +0530

    01.2 Apr

--- src/main/java/com/driver/Message.java
+++ src/main/java/com/driver/Message.java
@@ -5,7 +5,7 @@ import java.util.Date;
 public class Message {
     private int id;
     private String content;
-    //private Date timestamp;
+    private Date timestamp;
 
     public Message() {
     }
@@ -14,6 +14,11 @@ public class Message {
         this.id = id;
         this.content = content;
         //this.timestamp = timestamp;
+    }
+    public Message(int id, String content, Date timestamp) {
+        this.id = id;
+        this.content = content;
+        this.timestamp = timestamp;
     }
 
     public int getId() {
